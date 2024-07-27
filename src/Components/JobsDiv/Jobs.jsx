453e08d3297076fdcbccf71ca59c1488cba71273
Samuel Rodrigues <mrsam03102002@gmail.com>
2024-07-28 02:38:34 +0530

    halfway donw with job section

--- src/Components/JobsDiv/Jobs.jsx
+++ src/Components/JobsDiv/Jobs.jsx
@@ -1,8 +1,28 @@
-
+import { BiTimeFive } from "react-icons/bi"
 
 const Jobs = () => {
   return (
-    <div>Jobs</div>
+
+    <div>
+      <div className="jobContainer flex gap-10 justify-center flex-wrap items-center py-10" >
+        <div className="group group/item singleJob w-[250px] p-[20px] bg-white rounded [10px] hover:bg-blueColor shadow-lg shadow-greyIsh-400/700 hover:shadow-lg">
+          <span className='flex justify-between items-center gap-4'>
+            <h1 className='text-[16px] font-semibold text-[#252b36] group-hover:text-white' >Web Developer</h1>
+
+            <span className="flex items-center text-[#ccc] gap-1">
+              <BiTimeFive /> Now
+            </span>
+          </span>
+
+          <h6 className="text-[#ccc]">Canada</h6>
+
+          <p className='text-[13px] text-[#959595] pt-[20px] border-t-[2px] mt-[20px] group-hover:text-white'>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti, laboriosam!
+          </p>
+        </div>
+
+      </div >
+    </div >
   )
 }
 
