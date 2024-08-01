352483baddde98527ce2af2293d922963f5f9d31
Samuel Rodrigues <mrsam03102002@gmail.com>
2024-08-02 00:02:54 +0530

    ..

--- src/Components/JobsDiv/Jobs.jsx
+++ src/Components/JobsDiv/Jobs.jsx
@@ -1,25 +1,95 @@
 import { BiTimeFive } from "react-icons/bi"
+import logo1 from '../../Assets/logo1.png';
 
+
+const Data = [
+  {
+    id: 1,
+    image: logo1,
+    title: 'Web Developer',
+    time: 'Now',
+    location: 'USA',
+    desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corruptilaboriosam!',
+    company: 'Succedo Mortgage.'
+  },
+  {
+    id: 2,
+    image: logo1,
+    title: 'Web Developer',
+    time: 'Now',
+    location: 'USA',
+    desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corruptilaboriosam!',
+    company: 'Succedo Mortgage.'
+  },
+  {
+    id: 3,
+    image: logo1,
+    title: 'Web Developer',
+    time: 'Now',
+    location: 'USA',
+    desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corruptilaboriosam!',
+    company: 'Succedo Mortgage.'
+  },
+  {
+    id: 4,
+    image: logo1,
+    title: 'Web Developer',
+    time: 'Now',
+    location: 'USA',
+    desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corruptilaboriosam!',
+    company: 'Succedo Mortgage.'
+  },
+  {
+    id: 5,
+    image: logo1,
+    title: 'Web Developer',
+    time: 'Now',
+    location: 'USA',
+    desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corruptilaboriosam!',
+    company: 'Succedo Mortgage.'
+  },
+]
 const Jobs = () => {
   return (
 
     <div>
       <div className="jobContainer flex gap-10 justify-center flex-wrap items-center py-10" >
-        <div className="group group/item singleJob w-[250px] p-[20px] bg-white rounded [10px] hover:bg-blueColor shadow-lg shadow-greyIsh-400/700 hover:shadow-lg">
-          <span className='flex justify-between items-center gap-4'>
-            <h1 className='text-[16px] font-semibold text-[#252b36] group-hover:text-white' >Web Developer</h1>
+        {
+          Data.map(({ id, image, title, time, location, desc, company }) => {
+            return (
+              <div key={id} className="group group/item singleJob w-[250px] p-[20px] bg-white rounded [10px] hover:bg-blueColor shadow-lg shadow-greyIsh-400/700 hover:shadow-lg">
+                <span className='flex justify-between items-center gap-4'>
+                  <h1 className='text-[16px] font-semibold text-[#252b36] group-hover:text-white' >{title}</h1>
 
-            <span className="flex items-center text-[#ccc] gap-1">
-              <BiTimeFive /> Now
-            </span>
-          </span>
+                  <span className="flex items-center text-[#ccc] gap-1">
+                    <BiTimeFive /> {time}
+                  </span>
+                </span>
 
-          <h6 className="text-[#ccc]">Canada</h6>
+                <h6 className="text-[#ccc]">{location}</h6>
 
-          <p className='text-[13px] text-[#959595] pt-[20px] border-t-[2px] mt-[20px] group-hover:text-white'>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti, laboriosam!
-          </p>
-        </div>
+
+                <p className='text-[13px] text-[#959595] pt-[20px] border-t-[2px] mt-[20px] group-hover:text-white'>
+                  <h5 className="text-[#373737]">Job Description :</h5>
+                  {desc}
+                </p>
+
+                <div className="company flex items-center gap-2">
+                  <img src={image} alt="Company logo" className="w-[10%]" />
+
+                  <span className='text-[14px] py-[1rem] block group-hover:text-white'>{company}</span>
+                </div>
+
+
+                <button className='border-[2px] rounded-[10px] block p-[10px] w-full text- [14px] font-semibold text-textColor hover:bg-white group-hover/item:text-textColor group-hover:text-white'>
+                  Apply Now
+                </button>
+              </div>
+            )
+
+
+          })
+        }
 
       </div >
     </div >
