928c4781ce866f0825ecdb6e9038c43d40e7afe2
Samuel Rodrigues <mrsam03102002@gmail.com>
2024-08-07 19:23:06 +0530

    ...

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,21 +2,20 @@
 
 const Navbar = () => {
   return (
-    <div className='navBar flex justify-between items-center p-[3rem'>
+    <div className='navBar flex justify-between items-center p-[1.5rem]'>
       <div className='logoDiv'>
         <h1 className="logo text-[25px] text-blueColor">
-          <strong>Careers</strong>
+          <strong  className="text-red-600">CAREERS</strong> <strong className="text-blue-900">AT SUCCEDO</strong>
         </h1>
       </div>
 
       <div className="menu flex gap-8">
-        <li className="menuList text-[#6f6f6f] hover:text-blueColor">Jobs</li>
-        <li className="menuList text-[#6f6f6f] hover:text-blueColor">Companies</li>
-        <li className="menuList text-[#6f6f6f] hover:text-blueColor">About</li>
-        <li className="menuList text-[#6f6f6f] hover:text-blueColor">Contact</li>
-        <li className="menuList text-[#6f6f6f] hover:text-blueColor">Blog</li>
-        <li className="menuList text-[#6f6f6f] hover:text-blueColor">Login</li>
-        <li className="menuList text-[#6f6f6f] hover:text-blueColor">Register</li>
+        <li className="menuList text-[#6f6f6f] hover:text-blueColor p-4">Jobs</li>
+        
+        <li className="menuList text-[#6f6f6f] hover:text-blueColor p-4">About Us</li>
+        <li className="menuList text-[#6f6f6f] hover:text-blueColor p-4">Contact</li>
+      
+        <li className="menuList text-white  bg-blue-800 rounded-lg p-4">Register</li>
       </div>
     </div>
   )
--- src/Components/SearchDiv/Search.jsx
+++ src/Components/SearchDiv/Search.jsx
@@ -7,17 +7,17 @@ const Search = () => {
   return (
     <div className='searchDiv grid gap-10 bg-greyIsh rounded-[10px] p-[3rem]'>
       <form action="">
-        <div className='firstDiv flex justify-between items-center rounded-[8px] gap- [10px] bg-white p-5 shadow-lg shadow-greyIsh-700'>
+        <div className='firstDiv flex justify-between items-center rounded-[8px] gap- [10px] bg-blue-800 p-5 shadow-lg shadow-greyIsh-700'>
 
-          <div className='flex gap-2 items-center'>
-            <AiOutlineSearch className='text-[25px] icon' />
-            <input type="text" className='bg-transparent text-blue-500 focus:outline-none w-[100%]' placeholder='Search Jobs Here...' />
+          <div className='flex gap-5 items-center'>
+            <AiOutlineSearch className='text-[30px] icon text-white ' />
+            <input type="text" className='bg-transparent text-white focus:outline-none w-[100%]' placeholder='Search Jobs Here...' />
           </div>
 
        
 
 
-          <button className=' bg-blueColor h-full p-5 px-10 rounded-[10px] text-white cursor-pointer hover:bg-blue-300'>Search</button>
+          <button className=' bg-blueColor h-full p-5 px-10 rounded-[10px] text-white cursor-pointer hover:bg-blue-300'>GO</button>
 
         </div>
       </form>
