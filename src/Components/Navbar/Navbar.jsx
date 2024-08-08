9678ed74a283ad0dbf1f06375a50259161b28b28
achujebi <achujebi04@gmail.com>
2024-08-08 22:22:30 +0530

    text in black

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,25 +1,24 @@
-
-
 const Navbar = () => {
   return (
-    <div className='navBar flex justify-between items-center p-[3rem'>
+    <div className='navBar flex justify-between items-center p-[3rem]w-w-[1000px] mx-auto'>
+
       <div className='logoDiv'>
-        <h1 className="logo text-[25px] text-blueColor">
-          <strong>CAREERS AT SUCCEDO</strong>
+        <h1 className="logo text-[25px]"> 
+          <strong className="text-red-600">CAREERS</strong> <strong className="text-blue-600">AT SUCCEDO</strong>
         </h1>
       </div>
 
       <div className="menu flex gap-8">
-        <li className="menuList text-[#6f6f6f] hover:text-blueColor">Jobs</li>
-        <li className="menuList text-[#6f6f6f] hover:text-blueColor">Companies</li>
-        <li className="menuList text-[#6f6f6f] hover:text-blueColor">About</li>
-        <li className="menuList text-[#6f6f6f] hover:text-blueColor">Contact</li>
-        <li className="menuList text-[#6f6f6f] hover:text-blueColor">Blog</li>
-        <li className="menuList text-[#6f6f6f] hover:text-blueColor">Login</li>
-        <li className="menuList text-[#6f6f6f] hover:text-blueColor">Register</li>
+        <li className="menuList text-[#333] hover:text-blue-600">Home</li>
+        <li className="menuList text-[#6f6f6f] hover:text-blue-600">Jobs</li>
+        <li className="menuList text-[#6f6f6f] hover:text-blue-600">About US</li>
+        <li className="menuList text-[#6f6f6f] hover:text-blue-600">Contact</li>
+        <li className="menuList">
+          <button className="bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700">Register</button>
+        </li>
       </div>
     </div>
   )
 }
 
-export default Navbar
+export default Navbar;
